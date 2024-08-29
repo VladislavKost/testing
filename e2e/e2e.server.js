e0a09e39b1eb0df02de5a5ebb18774972dad1220
Владислав Костров <vladislav.kostrov@rydlab.ru>
2024-08-29 14:30:35 +0500

    Fix arguments

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const config = require("../webpack.dev");
 
-const server = new WebpackDevServer(webpack(config), {});
+const server = new WebpackDevServer({}, { compiler: webpack(config) });
 server.startCallback(() => {
   console.log("Webpack Dev Server is listening on port 8081");
   if (process.send) {
