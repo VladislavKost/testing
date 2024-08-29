329c16894c4365a064e9a2fc45ef40b07cebced7
Владислав Костров <vladislav.kostrov@rydlab.ru>
2024-08-29 14:42:44 +0500

    Update file

--- e2e/e2e.server.js
+++ e2e/e2e.server.js
@@ -2,9 +2,11 @@ const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const config = require("../webpack.dev");
 
-const server = new WebpackDevServer({ compiler: webpack(config) });
-server.startCallback(() => {
-  console.log("Webpack Dev Server is listening on port 8081");
+const server = new WebpackDevServer(webpack(config), {});
+server.listen(8081, "localhost", (err) => {
+  if (err) {
+    return;
+  }
   if (process.send) {
     process.send("ok");
   }
